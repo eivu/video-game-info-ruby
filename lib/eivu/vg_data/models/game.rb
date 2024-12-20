32e5a64a7cb0fb210162aa35b4cd06537ba3c418
Robert Jenkins <384403+dabobert@users.noreply.github.com>
2024-12-19 20:33:07 -0500

    added functionality to Game - created Game.extract_country - created Game.sanitize_rom

--- lib/eivu/vg_data/models/game.rb
+++ lib/eivu/vg_data/models/game.rb
@@ -1,10 +1,41 @@
 require 'active_record'
+require 'pry'
 
 module Eivu
   class VgData
     module Models
       class Game < ::ActiveRecord::Base
         include Eivu::VgData::Models::Concerns::ActiveRecordable
+
+        COUNTRY_REGEX = /\(([^)]+)\)/
+
+        class << self
+          def extract_country(rom_name)
+            case rom_name.scan(COUNTRY_REGEX).flatten&.first
+            when 'U'
+              'USA'
+            when 'J'
+              'Japan'
+            when 'E'
+              'Europe'
+            when 'K'
+              'Korea'
+            when nil
+              nil
+            else
+              'Unknown'
+            end
+          end
+
+
+          def sanitize_rom(rom_name)
+            name = File.basename(rom_name, '.*')
+            name = name.downcase
+            name = name.gsub(COUNTRY_REGEX, '')
+            name = name.gsub(/[^a-z0-9]/, '')
+            name
+          end
+        end
       end
     end
   end
