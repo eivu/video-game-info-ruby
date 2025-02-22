37b87f60461d478740fbf912e7fff40dce436d4b
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-02-22 13:58:50 -0500

    progress with specs

--- spec/lib/eivu/vg_data/models/game_spec.rb
+++ spec/lib/eivu/vg_data/models/game_spec.rb
@@ -28,4 +28,36 @@ describe Eivu::VgData::Models::Game do
       expect(described_class.extract_country('Super Mario Bros. (X)')).to eq('Unknown')
     end
   end
+
+  describe 'slugify_string' do
+    context 'with a normal string' do
+      it 'returns a slugified string' do
+        expect(described_class.slugify_string('Super Mario Bros.')).to eq('supermariobros')
+      end
+    end
+
+    context 'with a string with a country code' do
+      it 'returns a slugified string' do
+        expect(described_class.slugify_string('Super Mario Bros. (U)')).to eq('supermariobros')
+      end
+    end
+
+    context 'with a string that has values in brackets' do
+      it 'returns a slugified string' do
+        expect(described_class.slugify_string('Super Mario Bros. [misc]')).to eq('supermariobros')
+      end
+    end
+
+    context 'when the word the is in the middle of the string' do
+      it 'returns a slugified string' do
+        expect(described_class.slugify_string('Happy The Caveman')).to eq('happycaveman')
+      end
+    end
+
+    context 'when the word the is at the beginning of the string' do
+      it 'returns a slugified string' do
+        expect(described_class.slugify_string('The Happy Caveman')).to eq('happycaveman')
+      end
+    end
+  end
 end
