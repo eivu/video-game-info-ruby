85c3e5a93dfe47dd7ebe4c9839c6edd3f4ec8ca8
Rob Jenkins <384403+dabobert@users.noreply.github.com>
2025-02-22 13:06:27 -0500

    created spec helper and game_spec

---
+++ spec/lib/eivu/vg_data/models/game_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+describe Eivu::VgData::Models::Game do
+  describe 'extract_country' do
+    it 'returns USA for U' do
+      expect(described_class.extract_country('Super Mario Bros. (U)')).to eq('USA')
+    end
+
+    it 'returns Japan for J' do
+      expect(described_class.extract_country('Super Mario Bros. (J)')).to eq('Japan')
+    end
+
+    it 'returns Europe for E' do
+      expect(described_class.extract_country('Super Mario Bros. (E)')).to eq('Europe')
+    end
+
+    it 'returns Korea for K' do
+      expect(described_class.extract_country('Super Mario Bros. (K)')).to eq('Korea')
+    end
+
+    it 'returns Unknown for nil' do
+      expect(described_class.extract_country('Super Mario Bros.')).to eq('Unknown')
+    end
+
+    it 'returns Unknown for other' do
+      expect(described_class.extract_country('Super Mario Bros. (X)')).to eq('Unknown')
+    end
+  end
+end
